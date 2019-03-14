fdd25b3103b4a86328261b40cb3ad790f7ff3d83
mkubibaev <mkubibaev@gmail.com>
2019-03-14 19:58:15 +0600

    api adding new messages

--- server/app/messages.js
+++ server/app/messages.js
@@ -8,10 +8,18 @@ router.get('/', (req, res) => {
     res.send(fileDb.getItems());
 });
 
-// router.post('/', (req, res) => {
-//     req.body.id = nanoid();
-//     fileDb.addItem(req.body);
-//     res.send({message: 'OK'});
-// });
+router.post('/', (req, res) => {
+    if (!req.body.message) {
+        return res.status(400).send({"error": "message text is required!"});
+    } else if (!req.body.author) {
+        return res.status(400).send({"error": "author is required!"});
+    } else {
+        req.body.id = nanoid();
+        req.body.datetime = new Date().toISOString();
+        fileDb.addItem(req.body);
+        res.send({message: 'OK'});
+    }
+    
+});
 
 module.exports = router;
