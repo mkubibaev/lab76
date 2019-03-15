49a9c4321dd92cb6b05696147d60319587c801a0
Alina Bibikova <alinochkakleopatra@gmail.com>
2019-03-15 22:59:48 +0600

    added store

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,23 @@ import ReactDOM from 'react-dom';
 import MainContainer from './containers/MainContainer/MainContainer'
 import * as serviceWorker from './serviceWorker';
 
+import messageReducers from "./store/reducer/messaageReducers";
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-ReactDOM.render(<MainContainer />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(messageReducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+const mainContainer = (
+    <Provider store={store}>
+       <MainContainer />
+    </Provider>
+);
+
+ReactDOM.render(mainContainer, document.getElementById('root'));
 serviceWorker.unregister();
---
+++ client/src/store/reducer/messaageReducers.js
@@ -0,0 +1,11 @@
+const initialState = {
+    author: '',
+    message: '',
+    messages: []
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
